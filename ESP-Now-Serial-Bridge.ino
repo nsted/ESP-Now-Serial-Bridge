f18e8c012e80b7e86833082c4311cb0383c167cd
Nicholas Stedman <nickstedman@gmail.com>
2024-11-01 06:20:52 -0400

    Update from ESP-NOW IDF v4 to v5 libraries

--- ESP-Now-Serial-Bridge.ino
+++ ESP-Now-Serial-Bridge.ino
@@ -140,7 +140,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
 #endif
 
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const esp_now_recv_info *info, const uint8_t *incomingData, int len) { 
   #ifdef BLINK_ON_RECV
   digitalWrite(LED_BUILTIN, HIGH);
   #endif
